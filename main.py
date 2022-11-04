6a664378bf11d2d453ab8dc19a3989ba9ed66fa3
Julius Guay <JJGIV2010@gmail.com>
2022-11-04 08:46:11 -0500

    updated usd_discussion class with initial thoughts on using pages for variant sets as an example - need more time to write more

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pxr import Usd, UsdGeom, UsdPhysics, UsdShade, Sdf, Gf, Tf
 
 class USD_Discussion:
     def __init__(self):
+        self.git_url = "https://jjgiv2010.github.io/pxrd.github.io/"
         self.stage = omni.usd.get_context().get_stage()
 
         self.object_main_name = "example_main"
@@ -18,18 +19,15 @@ class USD_Discussion:
         ]
 
         self.variant_set_name_path = [
-            ("example_set_a", "variant_a", "/path_to_variant_a"),
-            ("example_set_a", "variant_b", "/path_to_variant_b"),
-            ("example_set_b", "variant_a", "/path_to_variant_a"),
-            ("example_set_b", "variant_b", "/path_to_variant_b")
-        ] # change to tuples and name files specifically
-
-
+            ("example_set_a", "variant_a"),
+            ("example_set_a", "variant_b"),
+            ("example_set_b", "variant_a"),
+            ("example_set_b", "variant_b")
+        ]
 
     def build_object(self):
         self.main_xform = UsdGeom.Xform.Define(self.stage, f"/{self.object_main_name}")
         self.main_prim = self.stage.DefinePrim(f"/{self.object_main_name}/{self.object_main_name}_Variants")
-
 
         for i in range(len(self.variant_sets)):
             variant_set = self.main_prim.GetVariantSets().AddVariantSet(self.variant_sets[i])
@@ -39,15 +37,12 @@ class USD_Discussion:
                 variant_set.SetVariantSelection(self.variant_set_name_path[i][1])
                 with variant_set.GetVariantEditContext():
                     new_variant_prim_path = self.stage.DefinePrim(self.main_prim.GetPath().AppendChild(self.variant_set_name_path[i][1]))
-                    new_variant_prim_path.GetReferences().AddReference(self.variant_set_name_path[i][2])
-
+                    new_variant_prim_path.GetReferences().AddReference(f"{self.git_url}/{self.object_main_name}/{self.variant_sets}/{self.variant_set_name_path[1]}.usda")
             else:
                 pass
 
-
-class Github_Connector:
-    def __init__(self):
+class Github_Object_Connector:
+    def __init__(self,object_name,variant_set,variant_name):
         self.git_url = "https://jjgiv2010.github.io/pxrd.github.io/"
-        self.parent_folder = ""
-        self.object_name = ""
-        self.object_path = f"{self.git_url}/{self.parent_folder}/{self.object_name}.usda"
+        self.variant_paths = f"{self.git_url}/{object_name}/{variant_set}/{variant_name}.usda"
+
