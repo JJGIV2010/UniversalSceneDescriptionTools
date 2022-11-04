d33966b19821a83b93a478524770b17eefac19fd
Julius Guay <JJGIV2010@gmail.com>
2022-11-04 09:48:30 -0500

    created a new omniverse tools class

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
-from pxr import Usd, UsdGeom, UsdPhysics, UsdShade, Sdf, Gf, Tf
+#from pxr import Usd, UsdGeom, UsdPhysics, UsdShade, Sdf, Gf, Tf
 
 class USD_Discussion:
     def __init__(self):
         self.git_url = "https://jjgiv2010.github.io/pxrd.github.io/"
-        self.stage = omni.usd.get_context().get_stage()
+        #self.stage = omni.usd.get_context().get_stage()
 
-        self.object_main_name = "example_main"
+        self.variant_dict = {
+            "name":"",
+            "variant_sets":[],
+            "variants":[],
+            "variant_custom_attributes":[],
+            "variant_transforms":[],
+            "variant_rotations":[],
+            "variant_scales":[],
+            "variant_pivots":[]
+        }
 
-        self.variant_sets = [
-            "example_set_a",
-            "example_set_b"
-        ]
+    def add_variant_set(self,variant_set):
+        self.variant_dict['variant_sets'].append(variant_set)
 
-        self.variant_set_name_path = [
-            ("example_set_a", "variant_a"),
-            ("example_set_a", "variant_b"),
-            ("example_set_b", "variant_a"),
-            ("example_set_b", "variant_b")
-        ]
+    def add_variant(self,variant_set,variant_name):
+        self.variant_dict['variants'].append((variant_set,variant_name))
 
-    def build_nested_usd_object(self):
-        self.main_xform = UsdGeom.Xform.Define(self.stage, f"/{self.object_main_name}")
-        self.main_prim = self.stage.DefinePrim(f"/{self.object_main_name}/{self.object_main_name}_Variants")
+    def create_new_nested_object(self,name,variant_sets,variants):
+        self.variant_dict['name'] = name
+        self.variant_dict['variant_sets'].append(variant_sets)
+        self.variant_dict['variants'].append(variants)
 
-        for i in range(len(self.variant_sets)):
-            variant_set = self.main_prim.GetVariantSets().AddVariantSet(self.variant_sets[i])
+    # def build_nested_object(self):
+    #     self.main_xform = UsdGeom.Xform.Define(self.stage, f"/{self.variant_dict['name']}")
+    #     self.main_prim = self.stage.DefinePrim(f"/{self.variant_dict['name']}/{self.variant_dict['name']}_Variants")
+    #
+    #     for i in range(len(self.variant_dict['variant_sets'])):
+    #         variant_set = self.main_prim.GetVariantSets().AddVariantSet(self.variant_dict['variant_sets'][i])
+    #
+    #         if self.variant_dict['variants'][i][0] == self.variant_dict['variant_sets'][i]:
+    #             variant_set.AddVariant(self.variant_dict['variants'][i][1])
+    #             variant_set.SetVariantSelection(self.variant_dict['variants'][i][1])
+    #
+    #             with variant_set.GetVariantEditContext():
+    #                 new_variant_prim_path = self.stage.DefinePrim(self.main_prim.GetPath().AppendChild(
+    #                     f"{self.variant_dict['variants'][i][1]}_mesh"))
+    #                 new_variant_prim_path.GetReferences().AddReference(
+    #                     f"{self.git_url}/{self.variant_dict['name']}/{self.variant_dict['variant_sets'][i]}/{self.variant_dict['variants'][i][0]}.usda")
+    #         else:
+    #             pass
 
-            if self.variant_set_name_path[i][0] == self.variant_sets[i]:
-                variant_set.AddVariant(self.variant_set_name_path[i][1])
-                variant_set.SetVariantSelection(self.variant_set_name_path[i][1])
+object_builder = USD_Discussion()
+object_builder.create_new_nested_object(
+    "food",
+    ["burritos","tacos"],
+    [
+        ("burritos","chicken"),
+        ("burritos","fish"),
+        ("burritos","carnitas"),
+        ("tacos", "steak"),
+        ("tacos", "chicken")
 
-                with variant_set.GetVariantEditContext():
-                    new_variant_prim_path = self.stage.DefinePrim(self.main_prim.GetPath().AppendChild(
-                        f"{self.variant_set_name_path[i][1]}_mesh"))
-                    new_variant_prim_path.GetReferences().AddReference(
-                        f"{self.git_url}/{self.object_main_name}/{self.variant_sets[i]}/{self.variant_set_name_path[1]}.usda")
-            else:
-                pass
+    ]
+
+)
