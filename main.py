91036daeff5d7214e654d600cde4ec73164f3f88
Julius Guay <JJGIV2010@gmail.com>
2022-11-04 22:39:48 -0500

    nice new variant set builder

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from pxr import Usd, UsdGeom, UsdPhysics, UsdShade, Sdf, Gf, Tf
 
 class USD_Discussion:
     def __init__(self):
-        self.git_url = "https://jjgiv2010.github.io/pxrd.github.io/"
+        self.main_directory_or_url = ""
         self.stage = omni.usd.get_context().get_stage()
         self.variant_dict = {
             "name":"",
             "variant_sets":[],
             "variants":[],
             "variant_custom_attributes":[]
+        }
+        self.sdf_dict = {
+            int: Sdf.ValueTypeNames.Int,
+            float: Sdf.ValueTypeNames.Float,
+            str: Sdf.ValueTypeNames.String
         }
 
     def add_comment(self,prim_path,text):
@@ -64,19 +69,20 @@ class USD_Discussion:
                             f"{self.variant_dict['variants'][k][1]}_mesh"))
                         self.set_kind(new_variant_prim.GetPath().pathString, "component")
                         self.add_comment(new_variant_prim.GetPath().pathString,
-                                         f"{self.variant_dict['variants'][k][1]} component, with {len(self.variant_dict['variant_custom_attributes'])} custom attributes ")
+                                         f"{self.variant_dict['variants'][k][1]} component custom attributes ")
 
                         new_variant_prim.GetReferences().AddReference(
-                           f"{self.git_url}/{self.variant_dict['name']}/{self.variant_dict['variant_sets'][i]}/{self.variant_dict['variants'][i][0]}.usda")
+                           f"{self.main_directory_or_url}/{self.variant_dict['name']}/{self.variant_dict['variant_sets'][i]}/{self.variant_dict['variants'][i][0]}.usda")
                         for a in range(len(self.variant_dict['variant_custom_attributes'])):
                             if self.variant_dict['variant_custom_attributes'][a][0] == self.variant_dict['variant_sets'][i]:
                                 if self.variant_dict['variant_custom_attributes'][a][1] == self.variant_dict['variants'][k][1]:
-                                    new_attribute = new_variant_prim.CreateAttribute(self.variant_dict['variant_custom_attributes'][a][2], Sdf.ValueTypeNames.String)
+                                    new_attribute = new_variant_prim.CreateAttribute(
+                                        self.variant_dict['variant_custom_attributes'][a][2],
+                                        self.sdf_dict[type(self.variant_dict['variant_custom_attributes'][a][3])])
                                     new_attribute.Set(self.variant_dict['variant_custom_attributes'][a][3])
 
                 else:
                     pass
-
 
 object_builder = USD_Discussion()
 
@@ -87,8 +93,8 @@ object_builder.create_new_nested_object("food",["burritos","tacos"],[
         ("tacos", "steak"),
         ("tacos", "chicken")
     ],[
-        ("burritos", "chicken","sauce","hot"),
-        ("burritos", "fish","sauce","mild"),
+        ("burritos", "chicken","test_int",10),
+        ("burritos", "fish","test_float",2.0),
         ("burritos", "carnitas","sauce","light"),
         ("tacos", "steak","sauce","none"),
         ("tacos", "chicken","sauce","super spicy")
